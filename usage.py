0f9113c1292faf02bea4406a6c316a9261924d0a
tinanemati <tinanemati.tina@gmail.com>
2023-12-18 14:43:48 -0800

    use react version 18 instead of 16

--- usage.py
+++ usage.py
@@ -1,10 +1,11 @@
 import well_plate
+import dash
 from dash import Dash, callback, html, Input, Output
 
 app = Dash(__name__)
 
 # Set React version inside the app
-Dash._dash_renderer._set_react_version("18.2.0")
+dash._dash_renderer._set_react_version("18.2.0")
 
 app.layout = html.Div([
     well_plate.WellPlate(
