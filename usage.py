de475f7e6d9ab1cf22dc279f445601cf2c47d6e1
tinanemati <tinanemati.tina@gmail.com>
2023-12-20 12:58:35 -0800

    added data for 384 well plate data

--- usage.py
+++ usage.py
@@ -7,32 +7,50 @@ app = Dash(__name__)
 # Set React version inside the app
 dash._dash_renderer._set_react_version("18.2.0")
 
-# Generate an array of well IDs (A1, A2, ..., H12)
-well_ids = []
+# Generate an array of well IDs (A1, A2, ..., H12) for 96 well-plate
+well_ids_96 = []
 for row in range(8):
     for col in range(1, 13):
         well_id = chr(65 + row) + str(col)
-        well_ids.append(well_id)
+        well_ids_96.append(well_id)
+
+# Generate an array of well IDs (A1, A2, ..., H12) for 96 well-plate
+well_ids_384 = []
+for row in range(16):
+    for col in range(1, 25):
+        well_id = chr(65 + row) + str(col)
+        well_ids_384.append(well_id)
 
 # Well Data
-wells_data = []
+wells_data_96 = []
+wells_data_384 = []
 
-
-rows = ["A", "B", "C", "D", "E", "F", "G", "H"]
-columns = [str(i) for i in range(1, 13)]
+rows_96 = ["A", "B", "C", "D", "E", "F", "G", "H"]
+columns_96 = [str(i) for i in range(1, 13)]
 count = 0
-for row in rows:
-    for col in columns:
+for row in rows_96:
+    for col in columns_96:
         well_id = row + col
         file_name = f"test {count}"
         count += 1
-        wells_data.append(
+        wells_data_96.append(
+            {"wellId": well_id, "fileName": file_name})
+
+rows_384 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P"]
+columns_384 = [str(i) for i in range(1, 25)]
+count = 0
+for row in rows_384:
+    for col in columns_384:
+        well_id = row + col
+        file_name = f"test {count}"
+        count += 1
+        wells_data_384.append(
             {"wellId": well_id, "fileName": file_name})
 
 app.layout = html.Div([
     WellPlate(
         id='my-wellplate-component',
-        WellsData=wells_data,
+        WellsData=wells_data_96,
     ),
     html.Div(id='output-div')
 ])
