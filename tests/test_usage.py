eb3d4ec5c79694f631728134631bdc60cb57355c
tinanemati <tinanemati.tina@gmail.com>
2023-12-22 19:32:17 -0800

    wrote a simple script for testing

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -1,25 +1,14 @@
 from dash.testing.application_runners import import_app
 
-
-# Basic test for the component rendering.
-# The dash_duo pytest fixture is installed with dash (v1.0+)
-def test_render_component(dash_duo):
+# Test for checking selectedWells output based on WellPlate interaction.
+def test_wellplate_output(dash_duo):
     # Start a dash app contained as the variable `app` in `usage.py`
     app = import_app('usage')
     dash_duo.start_server(app)
 
-    # Get the generated component input with selenium
-    # The html input will be a children of the #input dash component
-    my_component = dash_duo.find_element('#input > input')
+    # Simulate interaction with the 96-wellplate-input component
+    dash_duo.click('#96-wellplate-input > .well')  # Simulate a click on a well in the 96-wellplate
 
-    assert 'my-value' == my_component.get_attribute('value')
-
-    # Clear the input
-    dash_duo.clear_input(my_component)
-
-    # Send keys to the custom input.
-    my_component.send_keys('Hello dash')
-
-    # Wait for the text to equal, if after the timeout (default 10 seconds)
-    # the text is not equal it will fail the test.
-    dash_duo.wait_for_text_to_equal('#output', 'You have entered Hello dash')
+    # Check if the selectedWells output is as expected
+    expected_output = "You have selected the following wells in the 96 well-plate: A1"  # Change this according to your test
+    dash_duo.wait_for_text_to_equal('#96-wellplate-output', expected_output)
