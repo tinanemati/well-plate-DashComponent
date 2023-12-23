ca166d253c40670ca1e7a65927a435b523c4110d
tinanemati <tinanemati.tina@gmail.com>
2023-12-22 18:00:03 -0800

    react and dash component match right now

--- well_plate/WellPlate.py
+++ well_plate/WellPlate.py
@@ -20,16 +20,19 @@ Keyword arguments:
     The number used to identify the number of columns in the plate.
 
 - rows (number; required):
-    The number used to identify the number of rows in the plate."""
+    The number used to identify the number of rows in the plate.
+
+- selectedWells (list; optional):
+    The selected well in plate by user."""
     _children_props = []
     _base_nodes = ['children']
     _namespace = 'well_plate'
     _type = 'WellPlate'
     @_explicitize_args
-    def __init__(self, id=Component.UNDEFINED, WellsData=Component.REQUIRED, rows=Component.REQUIRED, columns=Component.REQUIRED, **kwargs):
-        self._prop_names = ['id', 'WellsData', 'columns', 'rows']
+    def __init__(self, id=Component.UNDEFINED, WellsData=Component.REQUIRED, rows=Component.REQUIRED, columns=Component.REQUIRED, selectedWells=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'WellsData', 'columns', 'rows', 'selectedWells']
         self._valid_wildcard_attributes =            []
-        self.available_properties = ['id', 'WellsData', 'columns', 'rows']
+        self.available_properties = ['id', 'WellsData', 'columns', 'rows', 'selectedWells']
         self.available_wildcard_properties =            []
         _explicit_args = kwargs.pop('_explicit_args')
         _locals = locals()
